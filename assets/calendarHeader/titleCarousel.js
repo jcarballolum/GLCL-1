8de5491b8fad5b1bc64efa686b7f6d4fb6a620d0
Josefa Carballo Lum <josefacarballolum@Josefas-MacBook-Air.local>
2018-09-23 09:18:41 -0700

    all committed

--- assets/calendarHeader/titleCarousel.js
+++ assets/calendarHeader/titleCarousel.js
@@ -1,46 +1,43 @@
-let calendarHeader = () => {
-    let title = document.getElementById('title');
-    let titleSetter = (newTitle) => {
-      title.innerHTML = newTitle;
-    };
-    titleSetter('SEPTEMBER');
-    let titleArr = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMEBER', 'DECEMBER'];
-    console.log(titleArr[0]);
+let titleCarouselWrapper = () => {
+  let titleCarousel = document.getElementById('title-carousel');
 
-    let titleCarousel = document.getElementById('title-carousel');
-    let left = document.createElement('p');
-    left.id = "leftArrow";
-    left.classList.add("arrow fas fa-chevron-left", "calendar-header", "title-carousel");
-    titleCarousel.appendChild(left);
+  let la = document.createElement('p');
+  la.classList.add('arrow', 'fas', 'fa-chevron-left');
+  la.id = "left-arrow";
+  titleCarousel.appendChild(la);
 
-    let right = document.createElement('p');
-    right.id = "rightArrow";
-    right.classList.add("arrow fas fa-chevron-right", "calendar-header", "title-carousel");
-    titleCarousel.appendChild(right);
+  let name = document.createElement('p');
+  name.classList.add('title');
+  name.id = "title";
+  titleCarousel.appendChild(name);
 
-    let month = document.createElement('p');
-    month.id = "title";
-    month.classList.add("calendar-header", "title-carousel");
-    titleCarousel.appendChild(month);
+  let ra = document.createElement('p');
+  ra.classList.add('arrow', 'fas', 'fa-chevron-right');
+  ra.id = "right-arrow";
+  titleCarousel.appendChild(ra);
 
-    let leftArrow = document.getElementById("left-arrow");
-    let rightArrow = document.getElementById('right-arrow');
-    let titleIndex = 8;
-    leftArrow.addEventListener('click', () => {
-      if (titleIndex > 0) {
-        titleIndex--;
-        titleSetter(titleArr[titleIndex]);
-      }
-    });
+  let title = document.getElementById('title');
+  let titleSetter = (newTitle) => {
+    title.innerHTML = newTitle;
+  };
+  titleSetter('SEPTEMBER');
+  let monthArr = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'];
 
-    righttArrow.addEventListener('click', () => {
-      if (titleIndex < titleArr.length-1) {
-        titleIndex++;
-        titleSetter(titleArr[titleIndex]);
-      }
-    });
+  let leftArrow = document.getElementById('left-arrow');
+  let rightArrow = document.getElementById('right-arrow');
+  let monthIndex = 8;
+  leftArrow.addEventListener('click', () => {
+    if (monthIndex > 0) {
+      monthIndex--;
+      titleSetter(monthArr[monthIndex]);
+    }
+  });
+  rightArrow.addEventListener('click', () => {
+    if (monthIndex < monthArr.length - 1) {
+      monthIndex++;
+      titleSetter(monthArr[monthIndex]);
+    }
 
-
-    };
-
-    document.addEventListener('DOMContentLoaded', calendarHeader, false);
+  });
+};
+document.addEventListener('DOMContentLoaded', titleCarouselWrapper, false);
